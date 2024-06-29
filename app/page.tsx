e0bed2edfb2a2e08f0d9263da08cb6e6736c4781
Nyagar Abraham <abrahamowino@gmail.com>
2024-06-29 21:54:47 +0300

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-	return (
-		<div>
-			<h1 className="h1-bold">next js 13</h1>
-			<h2 className="h2-bold">next js 13</h2>
-			<h3 className="h3-bold">next js 13</h3>
-		</div>
-	);
-}
